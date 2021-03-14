80fdba40bed22055bae7247bf198ad9ac56ebba9
build2learn <abdelwahed311@hotmail.com>
2021-03-14 19:12:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTestApp
+{
+    class Program
+    {
+        static int MissingNumber(int[] nums)
+        {
+            int sum = ((nums.Length )* (nums.Length + 1)) / 2;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum -= nums[i];
+            }
+            return sum;
+        }
+        public static void Main()
+        {
+            int[] a = { 3, 0, 1};
+            int miss = MissingNumber(a);
+            Console.Write(miss);
+            Console.ReadKey();
+        }
+        //static void Main(string[] args)
+        //{
+        //}
+    }
+}
